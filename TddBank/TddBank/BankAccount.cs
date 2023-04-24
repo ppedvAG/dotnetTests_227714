f6d218e0394b41772793331aa3bf3e52ba5d1079
André <AndreR@ppedv.de>
2023-04-24 14:35:34 +0200

    TDD BankAccount implementierung

--- TddBank/TddBank/BankAccount.cs
+++ TddBank/TddBank/BankAccount.cs
@@ -2,6 +2,25 @@
 {
     public class BankAccount
     {
-        public decimal Balance { get; }
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal value)
+        {
+            if (value <= 0)
+                throw new ArgumentException();
+
+            Balance += value;
+
+        }
+
+        public void Withdraw(decimal value)
+        {
+            if (value <= 0)
+                throw new ArgumentException();
+            if (value > Balance)
+                throw new InvalidOperationException();
+
+            Balance -= value;
+        }
     }
 }
