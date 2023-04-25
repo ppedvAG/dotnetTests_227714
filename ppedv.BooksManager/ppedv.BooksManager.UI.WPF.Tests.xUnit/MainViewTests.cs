766d00aca260bfc83d49068dbe9fe3c2b50c0519
André <AndreR@ppedv.de>
2023-04-25 15:17:37 +0200

    UI Test mit flaUI

---
+++ ppedv.BooksManager/ppedv.BooksManager.UI.WPF.Tests.xUnit/MainViewTests.cs
@@ -0,0 +1,31 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+using FluentAssertions;
+using ppedv.BooksManager.UI.WPF.Views;
+using Xunit;
+
+namespace ppedv.BooksManager.UI.WPF.Tests.xUnit
+{
+    public class MainViewTests
+    {
+        [Fact]
+        [Trait("", "UITest")]
+        public void Click_load_button_should_display_some_Books()
+        {
+            var appPath = typeof(MainPage).Assembly.Location.Replace(".dll", ".exe");
+            using var app = FlaUI.Core.Application.Launch(appPath);
+            using var automation = new UIA3Automation();
+
+            var win = app.GetMainWindow(automation);
+
+            var loadBtn = win.FindFirstDescendant(x => x.ByAutomationId("b1")).AsButton();
+            loadBtn.Click();
+
+            var grid = win.FindFirstDescendant(x => x.ByAutomationId("g1")).AsDataGridView();
+            grid.Rows.Length.Should().BeGreaterThan(1);
+
+            app.Close();
+        }
+    }
+}
+
